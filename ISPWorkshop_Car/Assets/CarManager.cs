eaa376f6035bd0c817cc807c350c8076837e6d3b
arthur1379 <changhsinlin1206@gmail.com>
2017-05-12 23:47:45 +0800

    0512

---
+++ ISPWorkshop_Car/Assets/CarManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CarManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Car;
+
+    [SerializeField]
+    Vector3 P1, P2;
+
+    [SerializeField]
+    Vector3[] OilPos;
+
+    Vector3 CarInsPos;
+
+    float timer;
+    float CarInstimer = 5.0f;
+
+    public Ease MoveEaseFunction; // 宣告Dotween函式庫中的Ease 名稱為RotateEaseFunction
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+
+        if (timer >= CarInstimer)
+        {
+            CarIns();
+        }
+	}
+
+    void CarIns()
+    {
+        timer = 0;
+
+        int Row = Random.Range(0, 1);
+        if (Row == 0)
+            CarInsPos = P1;
+        else
+            CarInsPos = P2;
+
+        GameObject m_Car = Instantiate(Car, CarInsPos, transform.rotation);
+
+        int RandomOilPos = Random.Range(0, 5);
+
+        m_Car.transform.DOMove(OilPos[RandomOilPos], 3);
+
+    }
+}
---
+++ ISPWorkshop_Car/Assets/readme.txt.txt
@@ -0,0 +1,4 @@
+/* NOTE
+this not plugins , it's only one basic way to make a path follow, have a advanced path follow  like curve , circle draw i will make the tutorial later, i hope understand what i send.
+ if you have any question about code , you can send the question for me via dunnoprice@gmail.com.
+*/
